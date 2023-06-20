3c5b0a886183588e6ccb67a59ab721e01e367536
megala <megha.krishna15@gmail.com>
2023-06-20 07:11:05 -0400

    modified

--- src/components/HeroImg3.js
+++ src/components/HeroImg3.js
@@ -22,19 +22,19 @@ const HeroImg3 = () => {
         <p data-aos="fade-right" className="text2">
           My expertise in project management also enables me to work effectively
           with design teams and deliver the final product on time.As a Front End
-          Developer in my current role, I collaborate closely with the design
-          team to understand the application features and user interface
-          requirements. Through extensive research, I gathered inspiration from
-          similar application designs and user interface modules, improving the
-          overall design. I am adept at creating website and application design
-          mockups, presenting updated front-end designs for final sign-off, and
-          writing front-end code to build the user interface. My core strengths
-          also include debugging code errors and troubleshooting software
-          issues, skills that are essential in delivering top-quality web
-          applications. With my considerable experience and skills, I am
-          confident in my ability to deliver the best results for your project.
+          Developer, I collaborate closely with the design team to understand
+          the application features and user interface requirements. Through
+          extensive research, I gathered inspiration from similar application
+          designs and user interface modules, improving the overall design. I am
+          adept at creating website and application design mockups, presenting
+          updated front-end designs for final sign-off, and writing front-end
+          code to build the user interface. My core strengths also include
+          debugging code errors and troubleshooting software issues, skills that
+          are essential in delivering top-quality web applications. With my
+          considerable experience and skills, I am confident in my ability to
+          deliver the best results for your project.
         </p>
-        <p className="text3">
+        {/* <p className="text3">
           Please refer to my{" "}
           <Link
             to="https://docs.google.com/document/d/1hKC2iOf7QKJN2k_k5I51--jzKmiZ25dI/edit"
@@ -45,7 +45,7 @@ const HeroImg3 = () => {
           </Link>{" "}
           for further details on my professional experience, skills, and
           qualifications. Thank you for considering my portfolio.
-        </p>
+        </p> */}
       </div>
     </div>
   );
